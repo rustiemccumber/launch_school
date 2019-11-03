4c0ae7c904784126fe598991c34a2d913fcd4c2c
Rustie McCumber <rustie.mccumber@gmail.com>
2019-11-03 14:10:32 -0300

    rock_paper_scissors_lizard_spock.rb after rubocop corrections

--- RB101/lesson_2/rock_paper_scissors_lizard_spock.rb
+++ RB101/lesson_2/rock_paper_scissors_lizard_spock.rb
@@ -14,7 +14,6 @@ WINNING_MOVES = {
   'sp' => ['sc', 'r']
 }
 
-
 def abbrv_key
   key = <<-KEY
   r..........rock        l........lizard
@@ -26,49 +25,44 @@ def abbrv_key
   puts key
 end
 
-
 def welcome
   puts "********************************************"
-  puts 
+  puts ""
   puts "welcome to rock, paper, scissor lizard spock"
-  puts " "
+  puts ""
   puts "********************************************"
-  puts " "
+  puts ""
   puts "the first player to score #{WINNING_SCORE} points is the grand winner!"
   puts ""
-end 
-
+end
 
 def win?(first, second)
   WINNING_MOVES[first].include?(second)
-end 
-
+end
 
 def clear_screen
   system("clear") || system("cls")
 end
 
 def display_score(score, player, computer)
-  displaying_score = <<-DSSC 
+  displaying_score = <<-DSSC
    player score : #{score[:player]}
    computer score: #{score[:computer]}
    tie: #{score[:tie]}
    DSSC
 
-   puts displaying_score 
+  puts displaying_score
 end
-
 
 def update_score(score, player, computer)
   if win?(player, computer)
     score[:player] += 1
   elsif win?(computer, player)
     score[:computer] += 1
-  else 
+  else
     score[:tie] += 1
   end
-end 
-
+end
 
 def gets_player_choice
   player_choice = ''
@@ -79,20 +73,20 @@ def gets_player_choice
       break
     else
       prompt("that's not a valid choice")
-    end 
+    end
   end
   player_choice
-end 
+end
 
 def play_for_grand_winner(score, player, computer)
-  if [:player] != WINNING_SCORE  && [:computer] != WINNING_SCORE
+  if [:player] != WINNING_SCORE && [:computer] != WINNING_SCORE
     prompt("would you like to continue playing for grand winner? (y, n)")
   end
-end 
+end
  
 def game_over?(score, player, computer)
   score[:player] == WINNING_SCORE || score[:computer] == 5
-end 
+end
 
 def game_over_winner(score, player, computer)
   if score[:player] == WINNING_SCORE
@@ -100,41 +94,34 @@ def game_over_winner(score, player, computer)
   elsif score[:computer] == WINNING_SCORE
     prompt("you are the grand LOSER!")
   end
-end 
-    
-
-
+end
 
 score = {
-  :player => 0,
-  :computer => 0,
-  :tie => 0
+  player: 0,
+  computer: 0,
+  tie: 0
 }
-continue_playing= ''
-loop do 
-
-  welcome 
+continue_playing = ''
+loop do
+  welcome
   abbrv_key
- 
   player_choice = gets_player_choice
   computer_choice = VALID_CHOICES.sample
-
   update_score(score, player_choice, computer_choice)
   display_score(score, player_choice, computer_choice)
   game_over_winner(score, player_choice, computer_choice)
 
-  if game_over?(score, player_choice,computer_choice)
+  if game_over?(score, player_choice, computer_choice)
     break
-  end 
+  end
 
   play_for_grand_winner(score, player_choice, computer_choice)
   continue_playing = gets.chomp
 
   if continue_playing == 'y'
     clear_screen
-  else 
-   break
+  else
+    break
   end
-
-
-end 
+end
+prompt("Thank you for playing")
