3921f93ad27f9c58c2a3191a48c27c18a2d9b557
Rustie McCumber <rustie.mccumber@gmail.com>
2020-05-09 10:23:39 -0700

    OO_twenty_one final

--- RB120/lesson_4/OO_Twenty_one.rb
+++ RB120/lesson_4/OO_Twenty_one.rb
@@ -72,31 +72,63 @@
 
 module Hand
 
+  def convert_deck_to_card_names
+    hand.map(&:to_s).join(' , ')
+  end
+
   def busted?
-    hand.values.add > 21
-  end 
+    total > 21
+  end
+
+  def convert_hand_to_values_array
+    hand.map(&:value)
+  end
+
+  def count_aces_in_hand
+    hand.count { |card| card.ace? }
+  end
+
+  def show_hand
+    puts "--- #{name}'s Hand ----"
+    puts convert_deck_to_card_names
+    puts ""
+    puts " => Total: #{total}"
+    puts ""
+  end
 
   def total
+    total = convert_hand_to_values_array.inject(:+)
+    #correct for aces
+    if total > 21
+      count_aces_in_hand.times do 
+        total -= 10
+        break if total <= 21
+      end 
+    end 
+    total
+  end
+
+  def reset_hand
+    @hand = []
   end
 
 end
 
 class Participant
+  include Hand
 
+  attr_accessor :hand, :name
   def initialize
-    @name
-    @hand = { }
+    @hand = []
   end
-
-  def hit
-  end 
-
-  def stay
-  end 
 
 end
 
 class Deck
+
+  MARKERS = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "Queen", "King" ]
+  SUITS = ["spades", "hearts", "clubs", "diamonds"]
+  VALUES = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10]
 
   attr_accessor :cards
   def initialize
@@ -104,19 +136,19 @@ class Deck
     load_deck
   end
 
-  def deal
-    # does the dealer or the deck deal?
+  def deal(player_type, number_of_cards)
+    number_of_cards.times do 
+      current_card = @cards.sample
+      @cards.delete(current_card)
+      player_type.hand << current_card
+    end 
+    #randomly select a card in the deck, and push it to the participants hand
   end 
 
   def load_deck
-
-    markers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "Queen", "King" ]
-    suites = ["spades", "hearts", "clubs", "diamonds"]
-    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10]
-
-    markers.zip(values).each do |marker, value|
-      suites.each do |suite|
-        @cards << Card.new( marker, suite, value)
+    MARKERS.zip(VALUES).each do |marker, value|
+      SUITS.each do |suit|
+        @cards << Card.new( marker, suit, value)
       end 
     end
   end
@@ -124,52 +156,215 @@ end
 
 class Player < Participant
 
-  include Hand
+ def set_name
+    name = ''
+    loop do
+      puts "What's your name?"
+      name = gets.chomp
+      break unless name.empty?
+      puts "Sorry, must enter a value."
+    end
+    self.name = name
+ end
 
-  attr_accessor :name
-  def initialize
-    @name
+ def show_flop
+    show_hand
+ end 
 
-  end
-
-
+  
 end 
 
 class Dealer < Participant
 
-  include Hand 
-
-  def deal
-    # does the dealer or the deck deal?
+  def initialize
+    super
+    set_name
   end 
+
+  ROBOTS = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5']
+
+  def set_name
+    self.name = ROBOTS.sample
+  end
+
+  def show_flop
+    puts "---- #{name}'s Hand ----"
+    puts "#{hand.first} and ??"
+    puts ""
+  end
 
 end
 
 class Card
-  def initialize( marker, suite, value)
+
+  attr_accessor :marker, :suit, :value
+  def initialize( marker, suit, value)
     @marker = marker
-    @suite =  suite
-    @value =  value
-  end 
+    @suit = suit
+    @value = value
+  end
+
+  def to_s
+    "#{marker} of #{suit}"
+  end
+  
+  def ace?
+    @marker == "Ace"
+  end
 end 
+
+# Here is an overview of the game:
+# - Both participcants are initially dealt 2 cards from a 52-card deck.
+# - The player takes the first turn, and can "hit" or "stay".
+# - If the player busts, he loses.  If he stays, it's the dealer's turn.
+# The dealer must hit until his cards add up to at least 17.
+# - If he busts, the player wins.  If bouth player and dealer stays, then the hightest total wins
+# - If both totals are equal, then it's a tie, and noby wins
+# - If he busts, the player wins.  If both player and dealer stays, then the highest total wins
+#- If both totals are equal, then it's a tie, and nobody wins.
 
 class Game
 
-  attr_accessor :deck
+  attr_accessor :deck, :dealer, :player
 
   def initialize
+    clear_screen
     @deck = Deck.new
+    @player = Player.new
+    @dealer = Dealer.new
   end
 
-  # def start
-  #   deal_cards
-  #   show_initial_cards
-  #   player_turn
-  #   delaer_turn
-  #   show_results
-  # end
+  def start
+    display_welcome_message
+    set_player_name
+    loop do
+      clear_screen
+      deal_initial_cards
+      show_initial_cards
+      player_turn
+      dealer_turn
+      show_results
+      play_again? ? reset : break 
+    end
+  end
+
+  def clear_screen
+    system 'clear'
+  end
+  
+  def set_player_name 
+    player.set_name
+  end
+
+  def display_welcome_message 
+    puts "Welcome to Twenty-One"
+  end
+
+  def deal_initial_cards
+    deck.deal(player, 2)
+    deck.deal(dealer, 2)
+  end
+
+  def show_initial_cards
+    player.show_flop
+    dealer.show_flop 
+  end
+
+  def show_player_cards
+    player.show_hand
+  end 
+
+  def hit_or_stay
+    puts "would you like to hit (h) or stay (s)?"
+    answer = nil
+    loop do
+      answer = gets.chomp.downcase
+      break if ["h", "s"].include?(answer)
+      puts "sorry that is not a valid input"
+    end
+    answer
+  end
+
+  def deal_to_player
+    deck.deal(player, 1)
+  end
+
+  def deal_to_dealer
+    deck.deal(dealer, 1)
+  end
+
+  def player_turn 
+    counter = 0 
+    loop do
+      clear_screen if counter >= 1
+      show_player_cards if counter >= 1
+      answer = hit_or_stay
+      deal_to_player if answer == "h"
+      break if player.busted?
+      break if answer == "s" || player.busted?
+      counter += 1
+    end
+  end
+
+  def dealer_turn
+    if !player.busted? && (dealer.total < 17)
+      loop do 
+        deal_to_dealer
+        break if dealer.busted? || (dealer.total >= 17)
+      end
+    end
+  end
+
+  def display_scoreboard
+    puts "player score: #{player.total}"
+    puts "dealer score: #{dealer.total}"
+  end
+
+  def display_busted
+    if player.busted?
+      puts "you busted dealer wins"
+    elsif dealer.busted?
+      puts "dealer busted you wins"
+    end 
+  end
+
+  def display_winner
+    if player.total > dealer.total 
+      puts "you won!"
+    elsif player.total < dealer.total
+      puts "dealer won!"
+    elsif player.total == dealer.total
+      puts "it's a tie"
+    end
+  end
+
+  def show_results
+    display_busted
+    if !player.busted? && !dealer.busted?
+      display_scoreboard
+      display_winner
+    end
+  end
+  
+  def play_again?
+    puts "would you like to play again yes (y) or no (n)?"
+    answer = nil
+    loop do
+      answer = gets.chomp.downcase 
+      break if ["y", "n"].include?(answer)
+    end
+    return false if answer == "n"
+    return true if answer == "y"
+  end
+
+  def reset
+     player.reset_hand
+     dealer.reset_hand
+     self.deck = Deck.new
+  end
+
 end
 
 #Game.new.start
 game_object = Game.new
-print game_object.deck.cards
+game_object.start
